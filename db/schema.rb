00699107b009f2ae3d1344917e3056cce58a2fbb
dabrickashaw60 <121248625+dabrickashaw60@users.noreply.github.com>
2024-07-11 23:55:54 -0400

    Onto step 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_11_183944) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_12_035228) do
+  create_table "attendances", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "attendee_id", null: false
+    t.index ["attendee_id"], name: "index_attendances_on_attendee_id"
+    t.index ["event_id"], name: "index_attendances_on_event_id"
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "date"
@@ -32,4 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_11_183944) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "attendances", "events"
+  add_foreign_key "attendances", "users"
+  add_foreign_key "attendances", "users", column: "attendee_id"
 end
